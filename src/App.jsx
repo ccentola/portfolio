a6b142d465429518155b62ac2b11620f6875a387
Carl Centola <carl.centola@gmail.com>
2023-08-17 19:07:52 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="name">Carl Centola</h2>
+        <p className="subtitle">Software Engineer</p>
+      </div>
+    </main>
+  );
+};
+export default App;
